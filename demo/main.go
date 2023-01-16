055f2f6819f9d80fdaf3aebaffc8fc3ebda84925
zfei <zfei@wooduan.com>
2023-01-16 14:45:47 +0800

    feat: grafana+prometheus

--- demo/main.go
+++ demo/main.go
@@ -2,7 +2,9 @@ package main
 
 import (
 	"fmt"
+	"reflect"
 	"strconv"
+	"strings"
 	"sync"
 	"time"
 )
@@ -58,6 +60,10 @@ func write2(name string) {
 	close(ch)
 }
 
+type AA struct {
+	A string
+}
+
 func main() {
 	//bytes := []byte("Hello Gophers!")
 	//s1, s2 := string(bytes), string(bytes)
@@ -87,4 +93,58 @@ func main() {
 
 	var s = "t"
 	fmt.Println(strconv.ParseBool(s))
+
+	//aa := "Hello"
+	//aa[0] = "T"
+	aa := strings.Split("a:b:c", ":")
+	bb := []string{"a", "b", "c"}
+	fmt.Println(reflect.DeepEqual(aa, bb))
+
+	m1 := make(map[string]string)
+	m2 := make(map[string]string)
+	m1["a"] = "a"
+	m2["a"] = "a"
+	// map 直接比较编译不通过  可以通过DeepEqual比较
+	fmt.Println(reflect.DeepEqual(m1, m2))
+	//fmt.Println(m1 == m2)
+
+	s1 := []string{
+		"1",
+	}
+	s2 := []string{
+		"1",
+	}
+
+	// slice 直接比较编译不通过  可以通过DeepEqual比较
+	fmt.Println(reflect.DeepEqual(s1, s2))
+	//fmt.Println(s1 == s2)
+
+	aa1 := AA{A: ""}
+	bb1 := AA{A: ""}
+	// struct 也可以通过DeepEqual比较 不过也可以直接使用==
+	fmt.Println(reflect.DeepEqual(aa1, bb1))
+	fmt.Println(aa1 == bb1)
+
+	// 还可以比较两个函数，仅支持有返回值的函数
+	fmt.Println(reflect.DeepEqual(f1(), f2()))
+
+	a1 := [1]string{"1"}
+	a2 := [1]string{"1"}
+	// 可以比较两个数组的类型和值  不过也可以使用==
+	fmt.Println(a1 == a2)
+	fmt.Println(reflect.DeepEqual(a1, a2))
+}
+
+type Interface1 interface {
+}
+
+type Interface2 interface {
+}
+
+func f1() int {
+	return 1
+}
+
+func f2() int {
+	return 1
 }
---
+++ grafana&&prometheus/README.md
@@ -0,0 +1,17 @@
+# grafana+prometheus
+Prometheus 是一套开源的监控 + 预警 + 时间序列数据库的组合，现在越来越多的公司或组织开始采用 Prometheus，现在常见的 kubernetes 容器管理系统，也会搭配 Prometheus 来进行监控。
+Prometheus 本身不具备收集监控数据功能，需要使用 http 接口来获取不同的 export 收集的数据，存储到时序数据库中。
+
+## 搭建环境
+- Prometheus： 普罗米修斯的主服务器,端口号9090
+- NodeExporter：负责收集Host硬件信息和操作系统信息，端口号9100
+- MySqld_Exporter：负责收集mysql数据信息收集，端口号9104
+- cAdvisor：负责收集Host上运行的docker容器信息,端口号占用8080
+- Grafana：负责展示普罗米修斯监控界面，端口号3000
+- altermanager：等待接收prometheus发过来的告警信息，altermanager再发送给定义的收件人
+
+## 安装grafana+prometheus
+[使用 docker 搭建 grafana+prometheus 监控服务器资源](https://blog.csdn.net/Song_Lun/article/details/120666421)
+[使用 docker 搭建 grafana+prometheus 监控数据库资源（贰）](https://blog.csdn.net/Song_Lun/article/details/120740732)
+[使用 docker 搭建 grafana+prometheus 监控docker资源（叁）](https://blog.csdn.net/Song_Lun/article/details/120777812)
+[使用 docker 搭建 grafana+prometheus+AlertManager 邮件报警（肆）](https://blog.csdn.net/Song_Lun/article/details/120748996)
